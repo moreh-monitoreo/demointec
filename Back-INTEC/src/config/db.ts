62d13f9aaca0b8d03a576cb7ef711ff87dff93ff
Edith Diaz Luna <edithdiazluna520@gmail.com>
2026-08-03 10:43:55 -0700

    fix: update SSL configuration to always set rejectUnauthorized to false

--- Back-INTEC/src/config/db.ts
+++ Back-INTEC/src/config/db.ts
@@ -51,7 +51,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  ssl: {
+    rejectUnauthorized: false
+  },
   synchronize: false,
   logging: false,
   entities: [UserEntity, RoleEntity, AttendanceEntity, ToolsEntity, MaterialEntity, ProjectEntity,
