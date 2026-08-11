57d66fc2a321aa08aaa47fe6ef270a3b976f9a4f
Edith Diaz Luna <edithdiazluna520@gmail.com>
2026-08-10 22:17:40 -0700

    fix: update SSL configuration to conditionally set rejectUnauthorized based on environment variable

--- Back-INTEC/src/config/db.ts
+++ Back-INTEC/src/config/db.ts
@@ -51,9 +51,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
   synchronize: false,
   logging: false,
   entities: [UserEntity, RoleEntity, AttendanceEntity, ToolsEntity, MaterialEntity, ProjectEntity,
